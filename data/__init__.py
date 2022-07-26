1703db8547c63d326454a52890b9220fbdd1dc59
dongwoohhh <dongwoo.lee@snu.ac.kr>
2022-07-26 13:10:01 +0900

    eval colmap data

--- data/__init__.py
+++ data/__init__.py
@@ -2,8 +2,10 @@ from .llff import LLFFDataset
 from .blender import BlenderDataset
 from .dtu_ft import DTU_ft
 from .dtu import MVSDatasetDTU
+from .colmap_ft import CustomDataset
 
 dataset_dict = {'dtu': MVSDatasetDTU,
                 'llff':LLFFDataset,
                 'blender': BlenderDataset,
-                'dtu_ft': DTU_ft}
+                'dtu_ft': DTU_ft,
+                'custom': CustomDataset,}
